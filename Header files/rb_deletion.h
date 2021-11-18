e3549a101af7cdeff56ea43fad2758616dfdf125
Sarthak Jain <332Sarthak22@gmail.com>
2021-11-18 21:33:45 +0530

    deleted redundant files

--- Header files/rb_deletion.h
+++
@@ -1,7 +0,0 @@
-#ifndef RB_DELETION_H_INCLUDED
-#define RB_DELETION_H_INCLUDED
-
-struct RBNode *deleteNode(struct RBNode *tree, struct RBNode *node_to_delete);
-struct RBNode *deleteFixUp(struct RBNode *tree, struct RBNode *node_to_fix);
-
-#endif
--- Header files/rb_insertion.h
+++
@@ -1,7 +0,0 @@
-#ifndef RB_INSERTION_H_INCLUDED
-#define RB_INSERTION_H_INCLUDED
-
-struct RBNode *insertNode(struct RBNode *, struct RBNode *);
-struct RBNode *insertFixUp(struct RBNode *tree, struct RBNode *node_to_fix);
-
-#endif
